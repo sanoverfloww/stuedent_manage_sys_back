08f92f1fab61483d9bd10ed9b9ce400a8c2035da
yifan <tyf528681034@mail.ustc.edu.cn>
2023-06-20 20:45:43 +0800

    DELETE student

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from flask import render_template, jsonify, request
 from app import app, mysql
+from flask_cors import CORS
+import json
+
+CORS(app)
 
 @app.route('/students')
 def get_students():
@@ -14,7 +18,7 @@ def get_students():
     students = []
     for row in result:
         student = {
-            'id': row[0],
+            'student_id': row[0],
             'name': row[1],
             'class': row[2],
             'major': row[3],
@@ -25,24 +29,52 @@ def get_students():
     # Return the data as JSON (In a real-world app, you might render it into an HTML template)
     return jsonify(students)
 
-@app.route('/students/<int:student_id>', methods=['PUT'])
+@app.route('/students/<string:student_id>', methods=['PUT'])
 def update_student(student_id):
-    # 获取请求的JSON数据
-    data = request.get_json()
+    # print(f"PUT request received for student_id: {student_id}")
+    try:
+        # 获取请求的JSON数据
+        raw_data = request.get_data(as_text=True)
+        
+        # 使用 json 模块解析 JSON 数据
+        data = json.loads(raw_data)
 
-    # 创建一个光标来与MySQL互动
-    cursor = mysql.get_db().cursor()
+        print(f"Parsed data is: {data}")
+        # 创建一个光标来与MySQL互动
+        cursor = mysql.get_db().cursor()
 
-    # 执行SQL查询来更新学生信息
-    query = """
-        UPDATE students
-        SET name=%s, class=%s, major=%s, college=%s
-        WHERE id=%s
-    """
-    cursor.execute(query, (data['name'], data['class'], data['major'], data['college'], student_id))
+        # 执行SQL查询来更新学生信息
+        query = """
+            UPDATE students
+            SET name=%s, class=%s, major=%s, college=%s
+            WHERE student_id=%s
+        """
+        cursor.execute(query, (data['name'], data['class'], data['major'], data['college'], student_id))
 
-    # 提交更改到数据库
-    mysql.get_db().commit()
+        # 提交更改到数据库
+        mysql.get_db().commit()
 
-    # 返回一个简单的响应
-    return jsonify({'message': 'Student updated successfully'})
+        # 返回一个简单的响应
+        return jsonify({'message': 'Student updated successfully'})
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return jsonify({'error': 'An error occurred processing the request'}), 500
+
+@app.route('/students/<string:student_id>', methods=['DELETE'])
+def delete_student(student_id):
+    try:
+        # 创建一个光标来与MySQL互动
+        cursor = mysql.get_db().cursor()
+
+        # 执行SQL查询来删除指定学生
+        query = "DELETE FROM students WHERE student_id=%s"
+        cursor.execute(query, (student_id,))
+
+        # 提交更改到数据库
+        mysql.get_db().commit()
+
+        # 返回一个简单的响应
+        return jsonify({'message': 'Student deleted successfully'}), 200
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return jsonify({'error': 'An error occurred processing the request'}), 500
